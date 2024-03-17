8a45609a52a7ff6b57d51f4d71aa1e7b5b1d3424
Mrx <MRX@email.com>
2024-03-17 20:34:00 +0330

    Comment serialazer and user filed added

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,19 +1,27 @@
 from rest_framework import serializers
 from .models import Comment
+from django.contrib.auth.models import User
+
+
+class UserSeralizer(serializers.ModelSerializer):
+    class Meta :
+        model = User
+        fields = ['username','email']
+
 
 
 
 
 class CommentSerializer(serializers.ModelSerializer):
 
+    user = UserSeralizer(read_only=True)
+
 
     class Meta:
         model = Comment
         fields = "__all__"
 
-
     
-
 
 
 
